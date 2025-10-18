521b2e15a78261b0cd3a782fdb72aeb825b9512e
Sri Krishna R Hebbar <srikrishnarhebbar1802@gmail.com>
2025-10-18 09:30:44 +0530

    Added the volunteers in Argonyx'24

---
+++ src/components/argonyx24/Volunteers.tsx
@@ -0,0 +1,36 @@
+import imageurl from "@/assets/argonyx24/volunteers.jpg";
+
+const Volunteer = () =>{
+   return(
+    <section>
+        <div className="max-w-xl mx-auto relative z-10">
+            <div className="text-center mb-16">
+                <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-orbitron font-black text-gradient mb-3 sm:mb-4">
+                    ARGONYX '24 Volunteers
+                </h2> 
+            </div>
+            <div 
+                key= "1"
+                className="glass-card overflow-hidden group hover-glow transition-all duration-500 hover:scale-105"
+            >
+                <div className="relative overflow-hidden">
+                <img 
+                    src= {imageurl}
+                    alt="volunteers24"
+                    className="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-background/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <div className="absolute bottom-4 left-4">
+                    <h3 className="text-lg font-orbitron font-semibold text-black dark:text-white">
+                        Volunteers 24
+                    </h3>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   );
+}
+
+export default Volunteer;
--- src/pages/Argonyx24.tsx
+++ src/pages/Argonyx24.tsx
@@ -4,6 +4,7 @@ import { About } from "@/components/argonyx24/About";
 import { Tracks } from "@/components/argonyx24/Tracks";
 import { Prizes } from "@/components/argonyx24/Prizes";
 import { Sponsors } from "@/components/argonyx24/Sponsors";
+import Volunteer from "@/components/argonyx24/Volunteers";
 
 const Argonyx24 = () => {
   return (
@@ -12,6 +13,7 @@ const Argonyx24 = () => {
       <Hero />
       <About />
       <Tracks />
+      <Volunteer/>
       <Prizes />
       <Sponsors />
     </div>
